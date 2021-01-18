fa28ccedfc82f1d99ccdbed2ed410d0ff71af364
demaio89@gmail.com <demaio89@gmail.com>
2021-01-18 12:27:03 -0600

    Add switch player logic

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 // setting up element selectors
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //works the same as queryselector, but preforms faster when dealing with thousands of elements.
 const currentScore0El = document.getElementById('current--0');
@@ -14,7 +16,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling Dice functionality
 const rollDice = function () {
@@ -29,9 +33,16 @@ const rollDice = function () {
   if (dice !== 1) {
     // add dice to current score
     currentScore += dice;
-    currentScore0El.textContent = currentScore; // Change Later;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
-    //   switch to next player
+    //   switch to next player and set current score to 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 };
 btnRoll.addEventListener('click', rollDice);
