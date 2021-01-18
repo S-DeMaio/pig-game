3b0b10b219931133ef6a8de387e84017fbf64df5
demaio89@gmail.com <demaio89@gmail.com>
2021-01-18 13:50:48 -0600

    Add hold button event listner functionality

--- script.js
+++ script.js
@@ -20,8 +20,17 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+// switch player function
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Rolling Dice functionality
-const rollDice = function () {
+btnRoll.addEventListener('click', function () {
   // generate random number
   const dice = Math.trunc(Math.random() * 6) + 1;
 
@@ -37,12 +46,20 @@ const rollDice = function () {
       `current--${activePlayer}`
     ).textContent = currentScore;
   } else {
-    //   switch to next player and set current score to 0;
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    //   switch to next player
+    switchPlayer();
   }
-};
-btnRoll.addEventListener('click', rollDice);
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. add current score to active player's score
+  scores[activePlayer] += currentScore;
+  // scores[1] = scores[1] + currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // 2. check if player's score is >= 100
+  // finish the game
+
+  // switch to next player
+  switchPlayer();
+});
