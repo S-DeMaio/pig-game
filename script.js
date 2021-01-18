2def9270d69491cffcc1a56ac0b2031c57c3af58
demaio89@gmail.com <demaio89@gmail.com>
2021-01-18 10:56:59 -0600

    Add player 1 current score counter

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // setting up element selectors
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //works the same as queryselector, but preforms faster when dealing with thousands of elements.
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +14,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling Dice functionality
 const rollDice = function () {
   // generate random number
@@ -20,6 +24,14 @@ const rollDice = function () {
   // display dice image
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-};
 
+  // check for rolled 1: if true, switch player
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    currentScore0El.textContent = currentScore; // Change Later;
+  } else {
+    //   switch to next player
+  }
+};
 btnRoll.addEventListener('click', rollDice);
